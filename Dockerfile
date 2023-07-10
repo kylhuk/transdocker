fdd493fa8eea1a14182db2c0ded9f3519b38ce28
kylhuk <kylhuk@c64.local>
2023-07-10 18:58:16 +0200

    should work, I guess

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+# Dockerfile
+
+# Set the base image
+FROM python:3.7
+
+# Set the working directory
+WORKDIR /app
+
+# Install necessary packages
+RUN apt-get update && apt-get install -y git
+
+# Copy the source code
+COPY . /app
+
+# Sync the github repo
+RUN git clone https://github.com/kylhuk/transdocker.git
+
+# Download the file
+# RUN wget <url>
+
+# Set environment variables
+ENV PYTHONPATH=/app
+ENV PYTHONUNBUFFERED=1
+
+# Install the dependencies
+RUN pip install -r requirements.txt
+
+# Start the python script
+CMD ["python", "server.py"]
+
+# Add labels
+LABEL maintainer="librehub <librehub@proton.me>"
+LABEL version="1.0"
---
+++ server.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+from flask import Flask, request
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def transdocker():
+    data = request.get_data(as_text=True)  # Get the input string from the request
+    response = translate(data)  # Call your helloWorld function with the input string
+    return response
+
+def translate(input_string):
+    # Your logic for processing the input string and generating the response
+    # For now, let's simply return a modified version of the input string
+
+    tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-de")
+    model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-de")
+
+    # Example input text
+    input_text = "Hello, how are you?"
+
+    # Tokenize the input text
+    input_tokens = tokenizer.encode(input_string, return_tensors="pt")
+
+    # Generate the translation
+    translated_tokens = model.generate(input_tokens)
+
+    # Decode the translated tokens back into text
+    translated_text = tokenizer.decode(translated_tokens[0], skip_special_tokens=True)
+
+    return translated_text
+
+    
